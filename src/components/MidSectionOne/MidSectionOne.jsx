f9bfff2190a8e54d5253507d30187e3f5186046a
DevanshR1123 <65907921+DevanshR1123@users.noreply.github.com>
2022-10-27 02:15:17 +0530

    Minor Changes

--- src/components/MidSectionOne/MidSectionOne.jsx
+++ src/components/MidSectionOne/MidSectionOne.jsx
@@ -1,26 +1,17 @@
 import React from "react";
-import { Grid, Container } from "@mui/material";
+import { Grid } from "@mui/material";
 import CardCustom from "../utils/CardCustom";
 
-const MidSectionOne = (props) => {
-
-    const {details, prizes, rules, venue} = props;
+const MidSectionOne = props => {
+  const { Details, Prizes, Rules, Venue } = props.Content;
 
   return (
-    <>
-      <Container
-        disableGutters
-        maxWidth="full"
-        sx={{ bgcolor: "background.main" }}
-      >
-        <Grid container columns={2} gap="3rem" p="2rem" justifyContent="center">
-          <CardCustom title="Details" content={details} />
-          <CardCustom title="Prizes" content={prizes} />
-          <CardCustom title="Rules" content={rules} />
-          <CardCustom title="Venue" content={venue} />
-        </Grid>
-      </Container>
-    </>
+    <Grid container columns={2} gap='3rem' p='2rem' justifyContent='center'>
+      <CardCustom title='Details' content={Details} />
+      <CardCustom title='Prizes' content={Prizes} />
+      <CardCustom title='Rules' content={Rules} />
+      <CardCustom title='Venue' content={Venue} />
+    </Grid>
   );
 };
 
