3beea5e3b8b3b886536a8a3e57c6bd5e0cf0b059
pratikwadke02 <82306536+pratikwadke02@users.noreply.github.com>
2022-10-28 21:23:53 +0530

    deploy warnings resolved

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,60 +8,60 @@ import InstagramIcon from "@mui/icons-material/Instagram";
 import LanguageIcon from '@mui/icons-material/Language';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
-const Wrapper=styled.section `
-.register{
-  max-width: 80vw;
-  margin: auto;
-  padding: 5rem 10rem;
-  border-radius: 1rem;
-  background-color: darkblue;}
+// const Wrapper=styled.section `
+// .register{
+//   max-width: 80vw;
+//   margin: auto;
+//   padding: 5rem 10rem;
+//   border-radius: 1rem;
+//   background-color: darkblue;}
 
-  .grid div:last-child{
-    justify-self: end;
-    align-self: center;
-  }
-  .footer-social-icons{
-    display: flex;
-    gap: 2rem;
-  }
-  .icons{
-    font-size: 2.4rem;
-    position: relative;
-    cursor: pointer;
-    height: 40px;
-      width: 40px;
-     background-color: #f5f6fa;
-     border-radius: 30px;
-    text-align: center;
-    margin: 10px;
-     box-shadow: 1px 4px 2px 2px;
-     line-height: 120px;
-  }
-  .butt{
-    cursor: pointer;
-    background-color: black;
-    color: yellowgreen;
-    border-radius: 5px;
-    transition: all 0.3s linear;
-    border: none;
-    font-size: 16px;
-    padding: 12px 70px;
-    background-image: url(background.jpg);
-  }
-  .container  grid grid-three-column{
-    display: flex;
-    flex-wrap: wrap;
-    background-color: #90ad70;
+//   .grid div:last-child{
+//     justify-self: end;
+//     align-self: center;
+//   }
+//   .footer-social-icons{
+//     display: flex;
+//     gap: 2rem;
+//   }
+//   .icons{
+//     font-size: 2.4rem;
+//     position: relative;
+//     cursor: pointer;
+//     height: 40px;
+//       width: 40px;
+//      background-color: #f5f6fa;
+//      border-radius: 30px;
+//     text-align: center;
+//     margin: 10px;
+//      box-shadow: 1px 4px 2px 2px;
+//      line-height: 120px;
+//   }
+//   .butt{
+//     cursor: pointer;
+//     background-color: black;
+//     color: yellowgreen;
+//     border-radius: 5px;
+//     transition: all 0.3s linear;
+//     border: none;
+//     font-size: 16px;
+//     padding: 12px 70px;
+//     background-image: url(background.jpg);
+//   }
+//   .container  grid grid-three-column{
+//     display: flex;
+//     flex-wrap: wrap;
+//     background-color: #90ad70;
     
-  }
-  .footer-social--icons{
-    display: flex;
-  }
-  .conts{
-    color: black;
-  }
+//   }
+//   .footer-social--icons{
+//     display: flex;
+//   }
+//   .conts{
+//     color: black;
+//   }
 
-`;
+// `;
 
 const footer = ()=>{
   const moreData = [
